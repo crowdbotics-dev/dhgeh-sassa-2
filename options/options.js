be6e84a0c7f2fc09e3723dd1fb47af525bef927a
Crowdbotics <engineering@crowdbotics.com>
2023-08-10 15:29:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dhgeh-sassa-2",
+  url: "https://dhgeh-sassa-2.botics.co",
+  api: "https://dhgeh-sassa-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
